40e3e6a5f286b787ae90471619dfc321c46c7a95
Georgy Grigoryev <iamkarlson@gmail.com>
2019-08-12 20:12:59 +0200

    variables

--- powershell/autorun/variables.ps1
+++ powershell/autorun/variables.ps1
@@ -1,7 +1,13 @@
 
 Set-Variable -Name Projects -Value "s:\work\" -Scope Global
 
-Set-Variable -Name Dropbox -Value "\\Mac\Dropbox\Apps" -Scope Global
+$envDrop = $env:Dropbox;
+if(Test-Path($envDrop)){
+    Set-Variable -Name Dropbox -Value "$envDrop\Apps" -Scope Global;
+} else {
+    Set-Variable -Name Dropbox -Value "\\Mac\Dropbox\Apps" -Scope Global;
+}
+
 
 Set-Variable -Name ntw -Value "$Projects\NT.Wallet" -Scope Global
 
