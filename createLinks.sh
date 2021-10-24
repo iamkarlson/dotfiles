7b0615dbf52fc695123745b7333c2669bbd09c06
Georgy Grigoryev <iamkarlson@gmail.com>
2021-10-24 18:19:20 +0200

    update migration script in linux

--- createLinks.sh
+++ createLinks.sh
@@ -1,20 +1,20 @@
 #!/bin/zsh
-ln -ds ~/dev/dotfiles/git/git ~/git
-ln -ds ~/dev/dotfiles/zsh/.zsh ~/.zsh
+ln -ds ~/src/dotfiles/git/git ~/git
+ln -ds ~/src/dotfiles/zsh/.zsh ~/.zsh
 
-ln -s ~/dev/dotfiles/git/system.gitconfig ~/.gitconfig
-ln -s ~/dev/dotfiles/vim/.vsvimrc ~/.vsvimrc
-ln -s ~/dev/dotfiles/vim/.vimrc ~/.vimrc
-ln -s ~/dev/dotfiles/zsh/.shellrc ~/.shellrc
-ln -s ~/dev/dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/src/dotfiles/git/system.gitconfig ~/.gitconfig
+ln -s ~/src/dotfiles/vim/.vsvimrc ~/.vsvimrc
+ln -s ~/src/dotfiles/vim/.vimrc ~/.vimrc
+ln -s ~/src/dotfiles/zsh/.shellrc ~/.shellrc
+ln -s ~/src/dotfiles/zsh/.zshrc ~/.zshrc
 
-ln -s ~/dev/dotfiles/.tmux.conf ~/.tmux.conf
-mkdir ~/.config
+ln -s ~/src/dotfiles/.tmux.conf ~/.tmux.conf
+mkdir -p ~/.config/nvim
 
-ln -s ~/dev/dotfiles/vim/init.vim ~/.config/nvim/init.vim
+ln -s ~/src/dotfiles/vim/init.vim ~/.config/nvim/init.vim
 mkdir ~/.vim
-ln -s ~/dev/dotfiles/vim/autocmd.vimrc ~/.vim/autocmd.vimrc
-ln -s ~/dev/dotfiles/vim/plugin-settings.vimrc ~/.vim/plugin-settings.vimrc
-ln -s ~/dev/dotfiles/vim/ide.vimrc ~/.vim/ide.vimrc
-ln -s ~/dev/dotfiles/vim/simple-mapping.vimrc ~/.vim/simple-mapping.vimrc
-ln -s ~/dev/dotfiles/vim/mapping.vimrc ~/.vim/mapping.vimrc
+ln -s ~/src/dotfiles/vim/autocmd.vimrc ~/.vim/autocmd.vimrc
+ln -s ~/src/dotfiles/vim/plugin-settings.vimrc ~/.vim/plugin-settings.vimrc
+ln -s ~/src/dotfiles/vim/ide.vimrc ~/.vim/ide.vimrc
+ln -s ~/src/dotfiles/vim/simple-mapping.vimrc ~/.vim/simple-mapping.vimrc
+ln -s ~/src/dotfiles/vim/mapping.vimrc ~/.vim/mapping.vimrc
--- git/.zsh
+++
@@ -1,1 +0,0 @@
-/home/iamkarlson/dev/dotfiles/zsh/.zsh
