7467a07f1493856ae5434fcd5b09cb6aae803757
George Green <iamkarlson@gmail.com>
2023-02-02 09:28:17 +0100

    vivaldi customization

--- vivaldi/main.js
+++ vivaldi/main.js
@@ -1,1 +1,20 @@
 // $("#main > div.inner > div:nth-child(2)").style = ""
+
+function mvMenu() {
+    const adr = document.querySelector('.UrlBar.toolbar');
+    const menu = document.querySelector('.vivaldi');
+	const tabs = document.querySelector('#tabs-container');
+	tabs.setAttribute('style','padding-left: 1px')
+    menu.setAttribute('style','position:static;height:var(--toolbarHeight);');
+    adr.appendChild(menu);
+};
+
+setTimeout(function wait() {
+    const browser = document.getElementById('browser');
+    if (browser) {
+        mvMenu();
+    }
+    else {
+        setTimeout(wait, 300);
+    }
+}, 300);
