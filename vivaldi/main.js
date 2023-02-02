d02f20502ad79fa875a06a80b3c62f5bc585dc0d
George Green <iamkarlson@gmail.com>
2023-02-02 10:17:39 +0100

    added vivaldi customization

--- vivaldi/main.js
+++ vivaldi/main.js
@@ -1,18 +1,30 @@
 // $("#main > div.inner > div:nth-child(2)").style = ""
 
+/*
+ *
+ * Vivaldi doesn't allow to inject any of javascript code directly.
+ * However, you can modify vivaldi browser files so it's using this script.
+ * > cp main.js /opt/vivaldi/resources/vivaldi/customization.js
+ */
+
 function mvMenu() {
-    const adr = document.querySelector('.UrlBar.toolbar');
+    const adr = document.querySelector('.mainbar > .toolbar');
     const menu = document.querySelector('.vivaldi');
 	const tabs = document.querySelector('#tabs-container');
 	tabs.setAttribute('style','padding-left: 1px')
     menu.setAttribute('style','position:static;height:var(--toolbarHeight);');
     adr.appendChild(menu);
+    document.querySelector("#header").remove()
 };
 
+console.log("customization script loaded");
+
 setTimeout(function wait() {
+    console.log("timer called");
     const browser = document.getElementById('browser');
     if (browser) {
         mvMenu();
+        console.log("moved!")
     }
     else {
         setTimeout(wait, 300);
