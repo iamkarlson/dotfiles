4faa4ac99a46987d6e095da542ee09307e23ceff
George Green <iamkarlson@gmail.com>
2023-05-31 21:46:57 +0200

    emacs stuff

--- doomemacs/config.el
+++ doomemacs/config.el
@@ -95,13 +95,11 @@
 (menu-bar-mode +1)
 (blink-cursor-mode +1)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(evil-respect-visual-line-mode t)
- '(org-insert-heading-respect-content t))
+ (setq-default evil-respect-visual-line-mode t)
+ (setq-default org-insert-heading-respect-content t)
+ (setq-default treemacs-follow-after-init t)
+ (setq-default treemacs-project-follow-cleanup t)
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
