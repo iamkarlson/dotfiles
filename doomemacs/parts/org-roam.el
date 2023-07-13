d0c5a7cfb6ecee003f26ca4a358e33c524fac026
George Green <iamkarlson@gmail.com>
2023-07-13 16:51:11 +0200

    emacs config

--- doomemacs/parts/org-roam.el
+++ doomemacs/parts/org-roam.el
@@ -27,7 +27,7 @@
           )
         org-roam-dailies-capture-templates
         '(("d" "default" entry "* %?"
-          :target (file+head "%<%Y-%m-%d>.org" "#+title: %<%B %d, %Y>\n\n* Goals for today\n- \n\n* Agenda \n- \n\n* Open tickets in [[https://gitlab.com/groups/dexter-energy/-/boards/2728779?assignee_username=georgydexter][gitlab]] \n- ")))))
+          :target (file+head "%<%Y-%m-%d>.org" "#+title: %<%B %d, %Y>\n\n* Goals for today\n- \n\n* Agenda \n- \n\n* Open tickets in [[https://gitlab.com/groups/dexter-energy/-/boards/2728779?assignee_username=georgydexter][gitlab]] \n- \n\n* Notes")))))
 
 
 
