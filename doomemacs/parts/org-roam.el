588b79f3d31a00cb39ef8f55c44f4e3b4906b892
George Green <iamkarlson@gmail.com>
2023-09-28 08:34:20 +0200

    added contact template

--- doomemacs/parts/org-roam.el
+++ doomemacs/parts/org-roam.el
@@ -6,12 +6,24 @@
 (setq org-roam-directory "~/braindb/")
 
 
+(defun my/org-roam-capture-contact ()
+  (let ((is-colleague (y-or-n-p "Is this a current colleague? ")))
+    (concat
+     (if is-colleague
+         "* My colleague from [[roam:Dexter Energy]]\n"
+       "")
+     "* %?")))
 
 (after! org-roam
   (setq org-roam-capture-templates
         `(
           ("d" "default" plain
            "* %?"
+           :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
+                              "#+title: ${title}\n")
+           :target (file "%<%Y%m%d%H%M%S>-${slug}.org")
+           :unnarrowed t)
+          ("c" "contact" plain (function my/org-roam-capture-contact)
            :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
                               "#+title: ${title}\n")
            :target (file "%<%Y%m%d%H%M%S>-${slug}.org")
@@ -27,7 +39,7 @@
           )
         org-roam-dailies-capture-templates
         '(("d" "default" entry "* %?"
-          :target (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y %B %d, %A>\n\n* Goals for today\n** \n\n* Agenda \n- \n\n* Open tickets in [[https://gitlab.com/groups/dexter-energy/-/boards/2728779?assignee_username=georgydexter][gitlab]] \n- \n\n* Notes")))))
+          :target (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y %B %d, %A, Week %V>\n\n* Goals for today\n** \n\n* Agenda \n- \n\n* Open tickets in [[https://gitlab.com/groups/dexter-energy/-/boards/2728779?assignee_username=georgydexter][gitlab]] \n- \n\n* Notes")))))
 
 
 
