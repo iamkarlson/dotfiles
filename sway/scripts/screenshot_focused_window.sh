280551f65441e91c39dc3f9f4ef1c6f39aaf4a5c
George Green <iamkarlson@gmail.com>
2024-01-04 23:20:30 +0100

    Added a nice notification for screenshots.
    Now it shows a notification with a preview, and if you click on it, it opens the screenshot in a default image viewer.

--- sway/scripts/screenshot_focused_window.sh
+++ sway/scripts/screenshot_focused_window.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
 #
 
-IMG_FILE=~/pictures/screenshots/$(date +"%Y%m%d%H%M%S").png;
+FILE_NAME=$(date +"%Y%m%d%H%M%S").png;
+IMG_FILE=~/pictures/screenshots/$FILE_NAME;
 grim -o "$(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | .name')" $IMG_FILE;
 $(wl-copy < $IMG_FILE);
+
+
+notify-send.sh "Screenshot is made!" "$FILE_NAME copied to clipboard!" -i "$IMG_FILE" -d "xdg-open $IMG_FILE";
