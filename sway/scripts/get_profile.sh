6c43893fb8680a117845591186231ff494425a41
George Green <iamkarlson@gmail.com>
2024-03-10 21:21:35 +0000

    smol laptop configuration

--- sway/scripts/get_profile.sh
+++ sway/scripts/get_profile.sh
@@ -5,6 +5,8 @@ if [ $hostname = "garnet" ]; then
     echo "home"
 elif [ $hostname = "beryl" ]; then
     echo "work"
+elif [ $hostname = "granite" ]; then
+    echo "laptop"
 else
     echo "CANNOT DETECT PROFILE" 2>&1 >>/tmp/log
 fi
