f6c7a3823a5b1d5293089747993f7afa0402a15b
George Green <iamkarlson@gmail.com>
2025-09-16 13:07:24 +0200

    doom dashboard

--- doomemacs/parts/ui.el
+++ doomemacs/parts/ui.el
@@ -120,6 +120,41 @@
   (insert "\n\n")
   )
 
+
+(setq-default +doom-dashboard-menu-sections
+              '(
+                ("Recently opened files"
+                 :icon (nerd-icons-faicon "nf-fa-file_text" :face 'doom-dashboard-menu-title)
+                 :action recentf-open-files)
+                ("Reload last session"
+                 :icon (nerd-icons-octicon "nf-oct-history" :face 'doom-dashboard-menu-title)
+                 :when (cond ((modulep! :ui workspaces)
+                              (file-exists-p (expand-file-name persp-auto-save-fname persp-save-dir)))
+                             ((require 'desktop nil t)
+                              (file-exists-p (desktop-full-file-name))))
+                 :action doom/quickload-session)
+                ("Open org-agenda"
+                 :icon (nerd-icons-octicon "nf-oct-calendar" :face 'doom-dashboard-menu-title)
+                 :when (fboundp 'org-agenda)
+                 :action org-agenda)
+                ("Open project"
+                 :icon (nerd-icons-octicon "nf-oct-briefcase" :face 'doom-dashboard-menu-title)
+                 :action projectile-switch-project)
+                ("Jump to bookmark"
+                 :icon (nerd-icons-octicon "nf-oct-bookmark" :face 'doom-dashboard-menu-title)
+                 :action bookmark-jump)
+                ("Open private configuration"
+                 :icon (nerd-icons-octicon "nf-oct-tools" :face 'doom-dashboard-menu-title)
+                 :when (file-directory-p doom-user-dir)
+                 :action doom/open-private-config)
+                ("[Open documentation]"
+                 :icon (nerd-icons-octicon "nf-oct-book" :face 'doom-dashboard-menu-title)
+                 :action doom/help)
+                )
+              )
+
+
+
 ;; Replace the shortmenu function in the dashboard functions list
 (setq +doom-dashboard-functions
       (append '(doom-dashboard-widget-banner)
