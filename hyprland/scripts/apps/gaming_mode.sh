6bfcc0e39e08745129cd27309c73bd2b9d807500
George Green <iamkarlson@gmail.com>
2025-10-26 20:41:44 +0100

    making arch great for gaming again

---
+++ hyprland/scripts/apps/gaming_mode.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Switch to tv and running steam
+#
+
+/home/iamkarlson/.config/hypr/scripts/monitors/switch-to-desktop.sh
+
+/home/iamkarlson/.config/hypr/scripts/apps/openfocus.sh steam
