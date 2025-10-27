37abb23f69f0880f30a4a26706d73c3f983c02c6
George Green <iamkarlson@gmail.com>
2025-10-27 16:03:19 +0100

    steam scripts and lock screen

--- hyprland/scripts/apps/gaming_mode.sh
+++ hyprland/scripts/apps/gaming_mode.sh
@@ -3,7 +3,7 @@
 # Switch to tv and running steam
 #
 
-/home/iamkarlson/.config/hypr/scripts/monitors/switch-to-desktop.sh
+/home/iamkarlson/.config/hypr/scripts/monitors/switch-to-tv.sh
 
 /home/iamkarlson/.config/hypr/scripts/apps/openfocus.sh steam
 
