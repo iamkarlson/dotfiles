790c5b819aefcce8fa4805b25d5b208b00ed9e8b
George Green <iamkarlson@gmail.com>
2025-12-25 19:31:58 +0100

    using resolution on TV

--- hyprland/scripts/monitors/switch-to-tv.sh
+++ hyprland/scripts/monitors/switch-to-tv.sh
@@ -2,7 +2,8 @@
 # Switch to TV (HDMI-A-2) and disable desktop monitor (HDMI-A-1)
 
 # Enable TV and disable desktop
-hyprctl keyword monitor "HDMI-A-2,preferred,auto,1"
+#hyprctl keyword monitor "HDMI-A-2,preferred,auto,1"
+hyprctl keyword monitor "HDMI-A-2,2560x1440@59.95Hz,auto,1"
 hyprctl keyword monitor "HDMI-A-1,disable"
 
 # Move all workspaces to TV
