73a8ef1ce2728aea8715243b230e420bc73edc19
George Green <iamkarlson@gmail.com>
2026-01-19 11:42:15 +0100

    hyprland and emacs stuff

--- hyprland/scripts/monitors/switch-to-tv.sh
+++ hyprland/scripts/monitors/switch-to-tv.sh
@@ -3,7 +3,7 @@
 
 # Enable TV and disable desktop
 #hyprctl keyword monitor "HDMI-A-2,preferred,auto,1"
-hyprctl keyword monitor "HDMI-A-2,2560x1440@59.95Hz,auto,1"
+hyprctl keyword monitor "HDMI-A-2,3840x2160@60.00Hz,auto,2"
 hyprctl keyword monitor "HDMI-A-1,disable"
 
 # Move all workspaces to TV
