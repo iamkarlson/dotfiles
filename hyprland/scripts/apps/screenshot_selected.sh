d2096b00aaf6c60d4c52b1abcbfa3418d299649c
George Green <iamkarlson@gmail.com>
2026-02-17 14:52:29 +0100

    refactored screenshot scripts

--- hyprland/scripts/apps/screenshot_selected.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-#
-IMG_FILE=~/pictures/screenshots/$(date +"%Y%m%d%H%M%S").png;
-grim -g "$(slurp)" $IMG_FILE;
-$(wl-copy < $IMG_FILE);
--- hyprland/scripts/apps/screenshot_selected_swappy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-#
-IMG_FILE=~/pictures/screenshots/$(date +"%Y%m%d%H%M%S").png
-grim -g "$(slurp)" $IMG_FILE
-notify-send.sh -u critical $IMG_FILE
-swappy -f $IMG_FILE -o $IMG_FILE
-wl-copy < $IMG_FILE
